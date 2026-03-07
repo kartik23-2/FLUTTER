0d7f74c8ea14bad7136f8385385659e7b7d37844
Kartik Baliyan <kartikbaliyan00004@gmail.com>
2026-03-07 20:47:05 +0530

    understanding widget chain

--- currency_converter/lib/main.dart
+++ currency_converter/lib/main.dart
@@ -1,122 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  ); // The runApp function takes the given Widget and makes it the root of the widget tree. In this case, we are passing an instance of MyApp, which is a StatelessWidget that we will define next. The const keyword is used to indicate that the MyApp widget is a compile-time constant, which can help with performance by allowing Flutter to optimize the widget tree.
 }
+// Widgets are the basic building blocks of a Flutter app's user interface. Each widget is an immutable declaration of part of the user interface. Unlike other frameworks that separate views, view controllers, layouts, and other properties, Flutter has a consistent, unified object model: the widget. A widget can define:
+// 1. Stateless widgets: These are immutable and do not have any internal state. They are typically used for static content that does not change over time.
+// 2. Stateful widgets: These can maintain internal state that may change during the lifetime of the widget.
+// 3. Inherited widgets: These are used to propagate information down the widget tree and allow child widgets to access that information.
+// 4. Layout widgets: These are used to arrange other widgets on the screen, such as Row, Column, Stack, etc.
+// 5. Input widgets: These are used to receive user input, such as TextField, Checkbox, etc.
 
+// Material design is a design system created by Google that provides a set of guidelines and components for building user interfaces. In Flutter, the MaterialApp widget is a convenience widget that wraps a number of widgets that are commonly required for material design applications. It provides features such as navigation, theming, and localization. By using MaterialApp, you can easily create a consistent look and feel for your app that follows the material design principles.
+// Cupertino design is a design system created by Apple that provides a set of guidelines and components for building user interfaces. In Flutter, the CupertinoApp widget is a convenience widget that wraps a number of widgets that are commonly required for Cupertino design applications. It provides features such as navigation, theming, and localization. By using CupertinoApp, you can easily create a consistent look and feel for your app that follows the Cupertino design principles.
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+  }); // This widget is the root of your application.this constructor is used to create an instance of the MyApp class. The super.key is a named parameter that allows you to pass a key to the parent class (StatelessWidget) constructor. A key is an identifier for widgets, elements, and semantic nodes. It is used to preserve the state of widgets when they are moved in the widget tree or when the widget tree is rebuilt. By passing the key to the parent class, you can ensure that the widget's state is preserved correctly during these operations.
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) {    // The build method describes how to display the widget in terms of other, lower-level widgets. The framework calls this method in a number of different situations. For example: a
+    return MaterialApp(      // The MaterialApp widget is a convenience widget that wraps a number of widgets that are commonly required for material design applications. It provides features such as navigation, theming, and localization. By using MaterialApp, you can easily create a consistent look and feel for your app that follows the material design principles.
+      home: Scaffold(
+        body: Center(child: Text('Hello World')),
+      ), // The home property of the MaterialApp widget is used to specify the default route of the app, which is displayed when the app is launched. In this case, we are setting it to a Text widget that displays "Hello World". This means that when the app is launched, it will show a screen with the text "Hello World" in the center.
+
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      //  home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
